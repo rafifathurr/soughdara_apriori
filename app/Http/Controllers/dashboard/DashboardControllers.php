2c4ba6d0ffbad39aadaa5ce9c5beabc1b4fead5a
rafifathur <rafifathur.rahman20@gmai.com>
2023-08-05 20:09:31 +0700

    Update Dashboard

--- app/Http/Controllers/dashboard/DashboardControllers.php
+++ app/Http/Controllers/dashboard/DashboardControllers.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\dashboard;
 use App\Http\Controllers\Controller;
 use App\Models\order\Order;
 use App\Models\order\Orders;
+use App\Models\order\detail\Details;
 use App\Models\product\Product;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
@@ -22,6 +23,10 @@ class DashboardControllers extends Controller
     // Index View and Scope Data
     public function index()
     {
+        date_default_timezone_set("Asia/Bangkok");
+        $yearz = date('Y');
+        $monthz = date('m');
+
         // SAMPLE
         $allorder = Order::whereRaw('MONTH(date) = MONTH(now())')
         ->select(DB::raw('product_id, sum(qty) as total'))
@@ -33,144 +38,42 @@ class DashboardControllers extends Controller
 
         // RETURN DATA
         $data['title'] = "Dashboard";
-        $data['orders'] = Order::all();
-        $data['dayofweeks'] = Order::whereRaw('date >= DATE(NOW() - INTERVAL 7 DAY)')
-                            ->where('deleted_at',null)
-                            ->orderBy('date', 'ASC')
-                            ->select(DB::raw('DATE_FORMAT(date, "%a") as name_day'))
-                            ->get();
-        $data['calofday'] = Order::whereRaw('date >= DATE(NOW() - INTERVAL 7 DAY)')
-                            ->where('deleted_at',null)
-                            ->select(DB::raw('count(*) as total'))
-                            ->orderBy('date', 'ASC')
-                            ->groupBy('date')
-                            ->get();
-        $data['profitmonth'] = Order::whereRaw('date <= NOW() and date >= Date_add(Now(),interval - 12 month)')
-                            ->where('deleted_at',null)
-                            ->select(DB::raw('sum(profit) as profit'))
-                            ->orderBy(DB::raw('DATE_FORMAT(date, "%b")'), 'ASC')
-                            ->groupBy(DB::raw('DATE_FORMAT(date, "%b")'))
-                            ->get();
-        $data['countorderall'] = count(Order::all()->where('deleted_at',null));
-        $data['countorderyear'] = count(Order::whereYear('date', Carbon::now()->year)->where('deleted_at',null)->get());
-        $data['countorderlastyear'] = count(Order::whereYear('date', (Carbon::now()->year)-1)->where('deleted_at',null)->get());
-        $data['countordermonth'] = count(Order::whereMonth('date', Carbon::now()->month)
-                                            ->where('deleted_at',null)
-                                            ->whereRaw('YEAR(date) = YEAR(now())')
-                                            ->get());
-        $data['countorderlastmonth'] = count(Order::whereMonth('date', Carbon::now()->month-1)
-                                                ->where('deleted_at',null)
-                                                ->whereRaw('YEAR(date) = YEAR(now())')
-                                                ->get());
-        $data['countorderday'] = count(Order::whereRaw('DAY(date) = DAY(now())')
-                                        ->where('deleted_at',null)
-                                        ->whereRaw('YEAR(date) = YEAR(now())')
-                                        ->get());
-        $data['countorderlastday'] = count(Order::whereRaw('DAY(date) = (DAY(now())-1)')
-                                            ->where('deleted_at',null)
-                                            ->whereRaw('YEAR(date) = YEAR(now())')
-                                            ->get());
-        $data['totalincome'] = Order::whereMonth('date', Carbon::now()->month)
-                                ->where('deleted_at',null)
-                                ->whereRaw('YEAR(date) = YEAR(now())')
-                                ->sum('entry_price');
-        $data['totalincomelast'] = Order::whereMonth('date', Carbon::now()->month-1)
-                                    ->where('deleted_at',null)
-                                    ->whereRaw('YEAR(date) = YEAR(now())')
-                                    ->sum('entry_price');
-        $data['totalprofit'] = Order::whereMonth('date', Carbon::now()->month)
-                                ->where('deleted_at',null)
-                                ->whereRaw('YEAR(date) = YEAR(now())')
-                                ->sum('profit');
-        $data['totalprofitlast'] = Order::whereMonth('date', Carbon::now()->month-1)
-                                    ->where('deleted_at',null)
-                                    ->whereRaw('YEAR(date) = YEAR(now())')
-                                    ->sum('profit');
-        $data['totaltax'] = Order::whereMonth('date', Carbon::now()->month)
-                            ->where('deleted_at',null)
-                            ->whereRaw('YEAR(date) = YEAR(now())')
-                            ->sum('tax');
-        $data['totaltaxlast'] = Order::whereMonth('date', Carbon::now()->month-1)
-                                ->where('deleted_at',null)
-                                ->whereRaw('YEAR(date) = YEAR(now())')
-                                ->sum('tax');
-        $data['incomepermonth'] = Order::whereYear('date', Carbon::now()->year)
-                                ->where('deleted_at',null)
-                                ->selectRaw('sum(entry_price) as income')
-                                ->groupBy(DB::raw('MONTH(date)'))
-                                ->orderBy(DB::raw('MONTH(date)'), 'ASC')
-                                ->get();
-
-        foreach($year as $y){
-            $data['years'][] = array(strval($y));
-            $checkstat = Order::whereYear('date', $y)
-                                ->where('deleted_at',null)
-                                ->selectRaw('sum(profit) as profit')
-                                ->orderBy(DB::raw('YEAR(date)'), 'ASC')
-                                ->groupBy(DB::raw('YEAR(date)'))
-                                ->first();
-            $profityear = Order::whereYear('date', $y)
-                                ->where('deleted_at',null)
-                                ->selectRaw('sum(profit) as profit')
-                                ->orderBy(DB::raw('YEAR(date)'), 'ASC')
-                                ->groupBy(DB::raw('YEAR(date)'))
-                                ->get();
-            if($checkstat){
-                foreach($profityear as $profit){
-                    if($profit->profit){
-                        $data['profityear'][] = array($profit->profit);
-                    }else{
-                        $data['profityear'][] = array('0');
-                    }
-                }
-            }else{
-                $data['profityear'][] = array('0');
-            }
-        }
 
         foreach($month as $mon){
             $dateObj   = DateTime::createFromFormat('!m', $mon);
             $monthName = $dateObj->format('F');
             $data['month'][]=array('month'=>$monthName);
-            $checkstat = Order::whereYear('date', Carbon::now()->year)
-                                ->where('deleted_at',null)
-                                ->whereMonth('date', $mon)
-                                ->selectRaw('sum(profit) as profit')
-                                ->orderBy(DB::raw('MONTH(date)'), 'ASC')
-                                ->groupBy(DB::raw('MONTH(date)'))
-                                ->first();
-            $profitmonth = Order::whereYear('date', Carbon::now()->year)
-                                ->where('deleted_at',null)
-                                ->whereMonth('date', $mon)
-                                ->selectRaw('sum(profit) as profit')
-                                ->orderBy(DB::raw('MONTH(date)'), 'ASC')
-                                ->groupBy(DB::raw('MONTH(date)'))
-                                ->get();
-            if($checkstat){
-                foreach($profitmonth as $profit){
-                    if($profit->profit){
-                        $data['profitpermonth'][] = array($profit->profit);
-                    }else{
-                        $data['profitpermonth'][] = array('0');
-                    }
-                }
+
+            $income = Orders::whereYear('date', $yearz)
+                    ->whereMonth('date', $mon)
+                    ->whereNull('deleted_at')
+                    ->where('orders_new.event_type', 'Payment')
+                    ->selectRaw('sum(total_amount) as income')
+                    ->groupBy(DB::raw('MONTH(date)'))
+                    ->first();
+
+            if($income){
+                $data['incomepermonth'][] = array($income->income);
             }else{
-                $data['profitpermonth'][] = array('0');
+                $data['incomepermonth'][] = array('0');
             }
         }
 
-        $data['topproduct'] = Order::whereYear('date', Carbon::now()->year)
-                                ->whereRaw('MONTH(date) = MONTH(now())')
-                                ->where('deleted_at',null)
-                                ->select(DB::raw('product_id, sum(qty) as total'))
-                                ->groupBy(DB::raw('product_id'))
-                                ->get();
-        $data['topsource'] = Order::whereYear('date', Carbon::now()->year)
-                                ->whereRaw('MONTH(date) = MONTH(now())')
-                                ->where('deleted_at',null)
-                                ->select(DB::raw('payment_method, count(*) as total'))
-                                ->groupBy(DB::raw('payment_method'))
-                                ->get();
+        $data['topproduct'] = Details::selectRaw('
+                                product.id,
+                                product.product_name,
+                                SUM(qty) as total
+                            ')
+                            ->join('orders_new', 'orders_new.id', '=', 'details_order.id_order')
+                            ->join('product', 'product.id', '=', 'details_order.id_product')
+                            ->whereYear('orders_new.date', $yearz)
+                            ->whereMonth('orders_new.date', $monthz)
+                            ->where('orders_new.event_type', 'Payment')
+                            ->groupBy('product.id')
+                            ->groupBy('product.product_name')
+                            ->orderBy('total', 'desc')
+                            ->limit(10)
+                            ->get();
 
         // RETURN VIEW
         return view('dashboard', $data);
